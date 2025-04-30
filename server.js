186582d025c653deb809b7d953da9099317b104e
hyxx7 <131360871+hyxx7@users.noreply.github.com>
2025-04-30 11:46:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname));
 
-app.use(express.static('public'));
+const users = {};
 
-let canvasState = [];
-let users = {};
+io.on("connection", socket => {
+  users[socket.id] = "Guest" + Math.floor(Math.random() * 1000);
+  io.emit("userList", Object.values(users));
 
-io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  const userColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  users[socket.id] = { id: socket.id, color: userColor };
-
-  socket.emit('init', { canvasState, users });
-  io.emit('user_list', users);
-  socket.broadcast.emit('user_joined', { id: socket.id, color: userColor });
-
-  socket.on('draw_pixel', (data) => {
-    canvasState.push(data);
-    socket.broadcast.emit('draw_pixel', data);
+  socket.on("draw", data => {
+    socket.broadcast.emit("draw", data);
   });
 
-  socket.on('cursor_move', (pos) => {
-    io.emit('cursor_update', { id: socket.id, pos });
+  socket.on("fill", data => {
+    socket.broadcast.emit("fill", data);
   });
 
-  socket.on('chat_message', (msg) => {
-    io.emit('chat_message', { id: socket.id, color: userColor, msg });
+  socket.on("cursor", ({ x, y }) => {
+    // (Optional: send live cursor positions here)
   });
 
-  socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
+  socket.on("chat", msg => {
+    io.emit("chat", users[socket.id] + ": " + msg);
+  });
+
+  socket.on("rename", name => {
+    users[socket.id] = name;
+    io.emit("userList", Object.values(users));
+  });
+
+  socket.on("disconnect", () => {
     delete users[socket.id];
-    io.emit('user_list', users);
-    io.emit('user_left', socket.id);
+    io.emit("userList", Object.values(users));
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+http.listen(3000, () => console.log("Server running on http://localhost:3000"));
