235db2a85386ab9c7ad387fe07a768bd151905c3
hyxx7 <131360871+hyxx7@users.noreply.github.com>
2025-05-01 13:11:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,70 +1,49 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static(__dirname + '/public'));
+const canvasData = [];
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.use("/socket.io", express.static(__dirname + "/node_modules/socket.io/client-dist"));
 
-let pixels = {};  // store drawn pixels
-let users = {};   // store connected users
+const users = new Map();
 
-io.on('connection', socket => {
-  console.log('A user connected:', socket.id);
+io.on("connection", (socket) => {
+  users.set(socket.id, { name: "Anonymous" });
+  io.emit("users", Array.from(users.values()));
 
-  // assign a random color and default name
-  users[socket.id] = {
-    name: 'Guest',
-    color: '#' + Math.floor(Math.random() * 16777215).toString(16)
-  };
-
-  // send existing pixels
-  socket.emit('init_pixels', pixels);
-
-  // update name
-  socket.on('set_name', name => {
-    users[socket.id].name = name;
+  socket.on("set_name", (name) => {
+    users.set(socket.id, { name });
+    io.emit("users", Array.from(users.values()));
   });
 
-  // draw pixel
-  socket.on('draw_pixel', data => {
-    const key = `${data.x},${data.y}`;
-    pixels[key] = data;
-    socket.broadcast.emit('draw_pixel', data);
+  socket.on("draw_pixel", ({ x, y, color }) => {
+    canvasData.push({ x, y, color });
+    socket.broadcast.emit("draw_pixel", { x, y, color });
   });
 
-  // cursor update
-  socket.on('cursor', pos => {
-    if (users[socket.id]) {
-      io.emit('cursor', {
-        id: socket.id,
-        name: users[socket.id].name,
-        color: users[socket.id].color,
-        x: pos.x,
-        y: pos.y
-      });
-    }
+  socket.on("get_canvas", () => {
+    socket.emit("canvas_data", canvasData);
   });
 
-  // chat
-  socket.on('chat', msg => {
-    if (users[socket.id]) {
-      io.emit('chat', {
-        name: users[socket.id].name,
-        msg
-      });
-    }
+  socket.on("chat", (msg) => {
+    const user = users.get(socket.id) || { name: "Unknown" };
+    io.emit("chat", user.name + ": " + msg);
   });
 
-  // disconnect
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('remove_cursor', socket.id);
-    console.log('User disconnected:', socket.id);
+  socket.on("disconnect", () => {
+    users.delete(socket.id);
+    io.emit("users", Array.from(users.values()));
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
