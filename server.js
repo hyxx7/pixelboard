1f5a0223735b4887a4ce34b229c20876f6d8431e
hyxx7 <131360871+hyxx7@users.noreply.github.com>
2025-05-01 12:54:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,69 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
-const users = {};
+app.use(express.static(__dirname));
 
-app
-::contentReference[oaicite:4]{index=4}
- 
+let pixels = {};  // store drawn pixels
+let users = {};   // store connected users
+
+io.on('connection', socket => {
+  console.log('A user connected:', socket.id);
+
+  // assign a random color and default name
+  users[socket.id] = {
+    name: 'Guest',
+    color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+  };
+
+  // send existing pixels
+  socket.emit('init_pixels', pixels);
+
+  // update name
+  socket.on('set_name', name => {
+    users[socket.id].name = name;
+  });
+
+  // draw pixel
+  socket.on('draw_pixel', data => {
+    const key = `${data.x},${data.y}`;
+    pixels[key] = data;
+    socket.broadcast.emit('draw_pixel', data);
+  });
+
+  // cursor update
+  socket.on('cursor', pos => {
+    if (users[socket.id]) {
+      io.emit('cursor', {
+        id: socket.id,
+        name: users[socket.id].name,
+        color: users[socket.id].color,
+        x: pos.x,
+        y: pos.y
+      });
+    }
+  });
+
+  // chat
+  socket.on('chat', msg => {
+    if (users[socket.id]) {
+      io.emit('chat', {
+        name: users[socket.id].name,
+        msg
+      });
+    }
+  });
+
+  // disconnect
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('remove_cursor', socket.id);
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
