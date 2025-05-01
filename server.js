4f459f9528b07e93037e72dcc43cdda9bc3e9479
hyxx7 <131360871+hyxx7@users.noreply.github.com>
2025-05-01 12:44:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-app.use(express.static(__dirname));
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const users = {};
 
-io.on("connection", socket => {
-  users[socket.id] = "Guest" + Math.floor(Math.random() * 1000);
-  io.emit("userList", Object.values(users));
-
-  socket.on("draw", data => {
-    socket.broadcast.emit("draw", data);
-  });
-
-  socket.on("fill", data => {
-    socket.broadcast.emit("fill", data);
-  });
-
-  socket.on("cursor", ({ x, y }) => {
-    // (Optional: send live cursor positions here)
-  });
-
-  socket.on("chat", msg => {
-    io.emit("chat", users[socket.id] + ": " + msg);
-  });
-
-  socket.on("rename", name => {
-    users[socket.id] = name;
-    io.emit("userList", Object.values(users));
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("userList", Object.values(users));
-  });
-});
-
-http.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app
+::contentReference[oaicite:4]{index=4}
+ 
